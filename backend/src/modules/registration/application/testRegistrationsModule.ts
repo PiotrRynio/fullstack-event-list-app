b131f42b13f301c13fd1b143b0716dabe6bee7cc
Piotr Rynio <pwrynio@gmail.com>
2022-02-01 00:50:11 +0100

    backend | fix InMemoryRegistrationsRepository name in testRegistrationsModule.ts

--- backend/src/modules/registration/application/testRegistrationsModule.ts
+++ backend/src/modules/registration/application/testRegistrationsModule.ts
@@ -1,9 +1,8 @@
 import { TestModuleCore, testModuleCore } from '../../../test-support/module/shared/core/TestModuleCore';
-import {RegistrationsModuleCore} from "../RegistrationsModuleCore";
-
+import { RegistrationsModuleCore } from '../RegistrationsModuleCore';
 
 export function testRegistrationsModule(currentTime: Date): TestModuleCore {
-  const registrationRepository = new InMemoryRegistrationRepository();
+  const registrationRepository = new InMemoryRegistrationsRepository();
 
   return testModuleCore((commandBus, eventBus, queryBus) =>
     RegistrationsModuleCore(eventBus, commandBus, () => currentTime, registrationRepository),
