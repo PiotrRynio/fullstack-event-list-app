ab29abd3d5f60e1bbffab57e53ff19eeb8ef231f
Piotr Rynio <pwrynio@gmail.com>
2022-02-02 00:31:51 +0100

    frontend | update TDD tests for Typography

--- frontend/src/components/Typography/Typography.spec.tsx
+++ frontend/src/components/Typography/Typography.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from 'test-utils';
+import { TypographyTag } from './TypographyTags';
 
 describe('Typography component', () => {
   const testText = 'test text';
@@ -27,7 +28,8 @@ describe('Typography component', () => {
     render(<Typography typographyTag={TypographyTag.HEADING_2}>{testText}</Typography>);
 
     // then
-    const textComponent = screen.getByText(/test text/i);
+    const textComponent = screen.getByRole('heading', { level: 2 });
+    expect(textComponent).toHaveTextContent(/test text/i);
     expect(textComponent).toBeInTheDocument();
   });
 });
