a856ff46ab934dd55b469897d1648a5abede07b5
Piotr Rynio <pwrynio@gmail.com>
2022-02-02 00:25:38 +0100

    frontend | add TDD tests for Typography

---
+++ frontend/src/components/Typography/Typography.spec.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from 'test-utils';
+
+describe('Typography component', () => {
+  const testText = 'test text';
+
+  it('should display REGULAR text, if component is rendered', () => {
+    // when
+    render(<Typography typographyTag={TypographyTag.REGULAR}>{testText}</Typography>);
+
+    // then
+    const textComponent = screen.getByText(/test text/i);
+    expect(textComponent).toBeInTheDocument();
+  });
+
+  it('should display OVERLINE text, if component is rendered', () => {
+    // when
+    render(<Typography typographyTag={TypographyTag.OVERLINE}>{testText}</Typography>);
+
+    // then
+    const textComponent = screen.getByText(/test text/i);
+    expect(textComponent).toBeInTheDocument();
+  });
+
+  it('should display HEADING_2 text, if component is rendered', () => {
+    // when
+    render(<Typography typographyTag={TypographyTag.HEADING_2}>{testText}</Typography>);
+
+    // then
+    const textComponent = screen.getByText(/test text/i);
+    expect(textComponent).toBeInTheDocument();
+  });
+});
---
+++ frontend/src/components/Typography/TypographyTags.ts
@@ -0,0 +1,5 @@
+export enum TypographyTag {
+  REGULAR = 'REGULAR',
+  OVERLINE = 'OVERLINE',
+  HEADING_2 = 'HEADING_2',
+}
