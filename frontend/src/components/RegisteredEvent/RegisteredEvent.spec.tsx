e551f9cc0f7622e0d9234451e1698f884021da08
Piotr Rynio <pwrynio@gmail.com>
2022-02-02 05:30:45 +0100

    frontend | refactor name from secondName for lastName

--- frontend/src/components/RegisteredEvent/RegisteredEvent.spec.tsx
+++ frontend/src/components/RegisteredEvent/RegisteredEvent.spec.tsx
@@ -4,17 +4,17 @@ import { RegisteredEvent } from './RegisteredEvent';
 
 describe('RegisteredEvent component', () => {
   const testFirstName = 'Jan';
-  const testSecondName = 'Kowalski';
+  const testLastName = 'Kowalski';
   const testEmail = 'test@test.com';
   const testDate = new Date('03 18 2021');
 
   it('should render correct', () => {
-    render(<RegisteredEvent firstName={testFirstName} secondName={testSecondName} email={testEmail} date={testDate} />);
+    render(<RegisteredEvent firstName={testFirstName} lastName={testLastName} email={testEmail} date={testDate} />);
   });
 
   it('should display event details, if component is rendered', () => {
     // given
-    render(<RegisteredEvent firstName={testFirstName} secondName={testSecondName} email={testEmail} date={testDate} />);
+    render(<RegisteredEvent firstName={testFirstName} lastName={testLastName} email={testEmail} date={testDate} />);
 
     // then
     screen.getByText(/Jan/i);
--- frontend/src/components/RegisteredEvent/RegisteredEvent.tsx
+++ frontend/src/components/RegisteredEvent/RegisteredEvent.tsx
@@ -3,18 +3,18 @@ import { TableCell, Wrapper } from './RegisteredEvent.styles';
 
 type RegisteredEventProps = {
   firstName: string;
-  secondName: string;
+  lastName: string;
   email: string;
   date: Date;
 };
-export const RegisteredEvent = ({ firstName, secondName, email, date }: RegisteredEventProps) => {
+export const RegisteredEvent = ({ firstName, lastName, email, date }: RegisteredEventProps) => {
   return (
     <Wrapper>
       <TableCell>
         <Typography typographyTag={TypographyTag.REGULAR}>{date.toLocaleDateString('en-CA')}</Typography>
       </TableCell>
       <TableCell>
-        <Typography typographyTag={TypographyTag.REGULAR}>{`${firstName} ${secondName}`}</Typography>
+        <Typography typographyTag={TypographyTag.REGULAR}>{`${firstName} ${lastName}`}</Typography>
       </TableCell>
       <TableCell>
         <Typography typographyTag={TypographyTag.REGULAR}>{email}</Typography>
