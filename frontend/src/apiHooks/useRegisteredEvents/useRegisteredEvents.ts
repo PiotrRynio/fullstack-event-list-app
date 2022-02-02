53be8b4c0b46459f1074bee2297d467601d4fd06
Piotr Rynio <pwrynio@gmail.com>
2022-02-02 09:05:57 +0100

    frontend | add REST_API_URL path in constants

--- frontend/src/apiHooks/useRegisteredEvents/useRegisteredEvents.ts
+++ frontend/src/apiHooks/useRegisteredEvents/useRegisteredEvents.ts
@@ -1,14 +1,15 @@
 import { useQuery, UseQueryResult } from 'react-query';
 import { validateResponse } from 'apiHooks/validateResponse';
-import { REGISTRATIONS_PATH } from 'constants/restApiPaths';
+import { REGISTRATIONS_PATH, REST_API_URL } from 'constants/restApiPaths';
 import { fromRegisteredEventsDto } from './fromRegisteredEventsDto';
 import { RegisteredEventsDto } from './RegisteredEventsDto';
 
 export const useRegisteredEvents = (): UseQueryResult<{}> =>
   useQuery([], async () => {
-    const response = await fetch(`${process.env.REST_API_URL}/${REGISTRATIONS_PATH}`);
+    const response = await fetch(`${REST_API_URL}${REGISTRATIONS_PATH}`);
     await validateResponse(response);
     const fetchedData: { registrations: RegisteredEventsDto[] } = await response.json();
+
     const registeredEvents = fetchedData.registrations.map(fromRegisteredEventsDto);
     return registeredEvents;
   });
--- frontend/src/constants/restApiPaths.ts
+++ frontend/src/constants/restApiPaths.ts
@@ -1,1 +1,2 @@
+export const REST_API_URL = 'http://localhost:5000/rest-api';
 export const REGISTRATIONS_PATH = '/registrations';
