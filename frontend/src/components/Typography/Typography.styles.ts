8e88cf3a4689b1f1516f3124c732285b562960f7
Piotr Rynio <pwrynio@gmail.com>
2022-02-02 02:01:35 +0100

    frontend | style typography

--- frontend/src/components/Typography/Typography.styles.ts
+++ frontend/src/components/Typography/Typography.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Regular = styled.p``;
-export const Overline = styled.p``;
-export const Heading2 = styled.h2``;
+export const Regular = styled.p`
+  ${({ theme }) => theme.mixins.typography.regular};
+`;
+export const Overline = styled.p`
+  ${({ theme }) => theme.mixins.typography.overline};
+`;
+export const Heading2 = styled.h2`
+  ${({ theme }) => theme.mixins.typography.heading2};
+`;
--- frontend/src/views/App/App.tsx
+++ frontend/src/views/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'assets/styles/GlobalStyles';
-import { theme } from 'assets/styles/theme';
+
 import Navbar from 'components/Navbar/Navbar';
 import Home from 'views/Home/Home';
 import { Wrapper } from './App.styles';
+import { theme } from 'assets/styles/theme';
 
 export const App = () => {
   return (
