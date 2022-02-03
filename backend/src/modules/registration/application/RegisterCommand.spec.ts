929fc616c3abf5d6f34982cb1fe3f0d4a8210e04
Piotr Rynio <pwrynio@gmail.com>
2022-02-03 16:34:24 +0100

    backend | add tests for RegisterCommand and Registration

--- backend/src/modules/registration/application/RegisterCommand.spec.ts
+++ backend/src/modules/registration/application/RegisterCommand.spec.ts
@@ -1,41 +1,214 @@
 import { CommandResult } from '../../../shared/Module/core/application/command/CommandResult';
 import Failure = CommandResult.Failure;
+import { HUNDRED_YEARS_IN_MILLISECONDS } from '../../../constants/times';
 import { testRegistrationsModule } from './testRegistrationsModule';
 import { RegisterCommand } from './RegisterCommand';
 
-describe('Registration | Register', () => {
-  it('given not existing registered item, when register, then registration were added', async () => {
-    //Given
-    const currentTime = new Date();
-    const registrationModule = testRegistrationsModule(currentTime);
-    const firstName = 'testFirstName';
-    const lastName = 'testLastName';
-    const userEmail = 'testUserEmail@test.com';
-    const userEventDate = new Date();
+describe('Registration |', () => {
+  const currentTime = new Date();
 
-    //When
-    const registerCommand = new RegisterCommand({ firstName, lastName: lastName, userEmail, userEventDate });
-    const commandResult = await registrationModule.executeCommand(registerCommand);
+  const defaultRegisterCommand = {
+    firstName: 'Adam',
+    lastName: 'Nowak',
+    userEmail: 'adamNowak@test.com',
+    userEventDate: new Date(),
+  };
 
-    //Then
-    expect(commandResult.isSuccess()).toBeTruthy();
+  describe('All |,', () => {
+    describe('when correct item is being registered,', () => {
+      it('then registration were added', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeTruthy();
+      });
+    });
   });
 
-  it('given not existing registered item, when register with invalid email, then throw error', async () => {
-    //Given
-    const currentTime = new Date();
-    const registrationModule = testRegistrationsModule(currentTime);
-    const firstName = 'testFirstName';
-    const lastName = 'testLastName';
-    const userEmail = 'testUserEmail';
-    const userEventDate = new Date();
+  describe('First Name |,', () => {
+    describe('when registered item is being registered,', () => {
+      it('if first name is too short, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const firstName = 'Ad';
 
-    //When
-    const registerCommand = new RegisterCommand({ firstName, lastName: lastName, userEmail, userEventDate });
-    const commandResult = await registrationModule.executeCommand(registerCommand);
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, firstName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
 
-    //Then
-    expect(commandResult.isSuccess()).toBeFalsy();
-    expect((commandResult as Failure).reason).toStrictEqual(new Error('Email is not correct.'));
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('First name must have at least 3 characters.'),
+        );
+      });
+
+      it('wif first name is too long, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const firstName = 'Addddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmm';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, firstName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('First name must have less then 20 characters.'),
+        );
+      });
+
+      it('if first name has not allowed character, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const firstName = 'Ad*am';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, firstName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(new Error('First name must have only letters.'));
+      });
+
+      it('if first name has space character, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const firstName = 'Ad am';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, firstName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(new Error('First name must have only letters.'));
+      });
+    });
+  });
+
+  describe('Last Name |,', () => {
+    describe('when registered item is being registered,', () => {
+      it('if last name is too short, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const lastName = 'No';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, lastName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('Last name must have at least 3 characters.'),
+        );
+      });
+
+      it('if last name is too long, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const lastName = 'Nooooooowwwwwwwaaaaaaaaaaaakkkkkkkkkk';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, lastName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('Last name must have less then 20 characters.'),
+        );
+      });
+
+      it('if last name has not allowed character, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const lastName = 'No&wak';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, lastName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(new Error('Last name must have only letters.'));
+      });
+
+      it('if last name has space character, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const lastName = 'Nowak  ';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, lastName });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(new Error('Last name must have only letters.'));
+      });
+    });
+  });
+
+  describe('Email |,', () => {
+    describe('when registered item is being registered,', () => {
+      it('if email is invalid, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const userEmail = 'adam@nowak@com.pl';
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, userEmail });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(new Error('Email is not correct.'));
+      });
+    });
+  });
+
+  describe('Event Date |,', () => {
+    describe('when registered item is being registered,', () => {
+      it('if data is before today, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const userEventDate = new Date(new Date().getTime() - HUNDRED_YEARS_IN_MILLISECONDS);
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, userEventDate });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('Date must be from now to a hundred years ahead.'),
+        );
+      });
+
+      it('if date is over a hundred years ahead, then throw error', async () => {
+        //Given
+        const registrationModule = testRegistrationsModule(currentTime);
+        const userEventDate = new Date(new Date().getTime() + HUNDRED_YEARS_IN_MILLISECONDS * 2);
+
+        //When
+        const registerCommand = new RegisterCommand({ ...defaultRegisterCommand, userEventDate });
+        const commandResult = await registrationModule.executeCommand(registerCommand);
+
+        //Then
+        expect(commandResult.isSuccess()).toBeFalsy();
+        expect((commandResult as Failure).reason).toStrictEqual(
+          new Error('Date must be from now to a hundred years ahead.'),
+        );
+      });
+    });
   });
 });
