42159c574b3d5cb3d58b093afc1d151a3fe07c26
Piotr Rynio <pwrynio@gmail.com>
2022-02-03 12:50:34 +0100

    frontend | refactor regex patterns

--- frontend/src/constants/regexPatterns.ts
+++ frontend/src/constants/regexPatterns.ts
@@ -1,4 +1,4 @@
 export const EMAIL_REGEX_PATTERN = /^[A-Z0-9._%+-]+@[A-Z]+\.[A-Z][A-Z]/i;
 export const DATE_REGEX_PATTERN = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-export const FIRST_NAME_REGEX_PATTERN = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/i;
-export const LAST_NAME_REGEX_PATTERN = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/i;
+export const FIRST_NAME_REGEX_PATTERN = /^[a-z]{3,}$/i;
+export const LAST_NAME_REGEX_PATTERN = /^[a-z]{3,}$/i;
